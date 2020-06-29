9d214116c613151653a1238e1824e9b0ffe0d564
Dillon Tryhorn <dillontryhorn@gmail.com>
2020-06-29 13:44:42 -0400

    Got some derranged version with omp working.

--- src/ParallelMDFS.cpp
+++ src/ParallelMDFS.cpp
@@ -5,6 +5,8 @@
 #include <tuple>
 #include <bits/stdc++.h> 
 
+std::atomic<bool> success(false);
+
 bool ParallelMDFS::Execute(int x, int y)
 {
     if(this->goalTile != nullptr)
@@ -23,14 +25,16 @@ bool ParallelMDFS::Execute(int x, int y)
             for(auto neighbor : neighbors)
                 hTups.emplace_back(neighbor->getHeuristic(), neighbor);
             std::sort(hTups.begin(), hTups.end());
-            for( auto tup : hTups )
+            #pragma omp parallel for num_threads(this->num_threads)
+            for(auto it = hTups.begin(); it < hTups.end(); it++)
             {
-                if(std::get<1>(tup)->getID() != 2) //don't cross into enemy node
+                if(success) continue;
+                if(std::get<1>(*it)->getID() != 2) //don't cross into enemy node
                 {
                     std::vector<std::shared_ptr<Tile>> localSP;
                     localSP.emplace_back(startingTile);
-                    if(ParallelMDFS::Worker(std::get<1>(tup), localSP) == 1) //goal node found
-                        return true;
+                    if(ParallelMDFS::Worker(std::get<1>(*it), localSP) == 1) //goal node found
+                        success = true;
                 }
             }
         }
@@ -50,21 +54,27 @@ int ParallelMDFS::Worker(std::shared_ptr<Tile> tile, std::vector<std::shared_ptr
         if(tile->getID() == 1) //goal node
         {
             this->searchPath = localSP;
-            return 1;
+            success = true;
         }
         auto neighbors = this->tilegraph->GetNeighbors(tile);
         std::vector<std::tuple<int, std::shared_ptr<Tile>>> hTups;
         for(auto neighbor : neighbors)
             hTups.emplace_back(neighbor->getHeuristic(), neighbor);
         std::sort(hTups.begin(), hTups.end());
-        for(auto tup : hTups)
+        #pragma omp parallel for num_threads(this->num_threads)
+        for(auto it = hTups.begin(); it < hTups.end(); it++)
         {
-            if(std::get<1>(tup)->getID() != 2 && !std::get<1>(tup)->isDiscovered()) //don't cross into enemy node
+            if(success) continue;
+            if(std::get<1>(*it)->getID() != 2 && !std::get<1>(*it)->isDiscovered()) //don't cross into enemy node
             {
-                if(ParallelMDFS::Worker(std::get<1>(tup), localSP) == 1) //goal node found
-                    return 1;
+                if(ParallelMDFS::Worker(std::get<1>(*it), localSP) == 1)
+                    success = true;
+                //auto future = this->pool->submit(&ParallelMDFS::Worker, std::get<1>(tup), localSP);
+                //if(future.get() == 1) //goal node found
+                //    return 1;
             }
         }
+        if(success) return 1;
     }
     return 0;
 }
